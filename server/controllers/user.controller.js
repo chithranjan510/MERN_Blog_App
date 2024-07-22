7b8127cf8d2d76285ff714476ee0d34b19d3c9bf
chithranjan kumar <kumarchithranjan@gmail.com>
2024-07-22 16:29:45 +0530

    fix

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -27,7 +27,7 @@ export const login = async (req, res) => {
         res
           .status(200)
           .cookie("token", token, {
-            domain: "https://mern-blog-app-frontend-g6t6.onrender.com",
+            domain: ".onrender.com",
             maxAge: 24 * 60 * 60 * 1000,
             httpOnly: true,
             secure: true,
